dac851e8225a00b9e092ac7613636eeb7def294b
Prajeesha <praji22@gmail.com>
2022-11-29 17:50:25 +0530

    login and redirection done for the todo list

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-
 function validate(callback){
   let username = document.getElementById("username");
   let password = document.getElementById("password");
-    if(username.value=="admin" && password.value==12345){
-      alert("Valid Name and password");
-      true;
+    if(username.value == "admin" && password.value == "12345"){
       callback();
      }
     else{
@@ -17,3 +14,28 @@ function redirect(){
   window.location.href="todo.html";
 }
 validate(redirect);
+
+function displaylist(){
+  let request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+      let response = JSON.parse(this.responseText);
+      let printlist = '';
+      console.log(response.length)
+      for(var i=0; i<response.length; i++){
+          printlist += `<table>
+                      <tr>
+                        <td> ${response[i].id} </td>
+                        <td> ${response[i].title} </td>
+                        <td> <input type="checkbox" id="myCheck" value=${i} ${response[i].completed?"checked disabled":"unchecked"}></td>
+                      </tr>
+                      </table>`;        
+                      
+      }
+      document.getElementById("showlist").innerHTML = printlist;
+    }
+  }
+  request.open("GET","https://jsonplaceholder.typicode.com/todos")
+  request.send()
+}
+
